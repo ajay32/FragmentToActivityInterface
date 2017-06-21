2802dcda4ffade51f2ca9546e47b88a11f379194
Ajay Mehta <dev.ajaymehta@gmail.com>
2017-06-21 07:57:44 -0700

    Added Why

--- app/src/main/java/ajaymehta/communicationwithinterface/MainActivity.java
+++ app/src/main/java/ajaymehta/communicationwithinterface/MainActivity.java
@@ -9,6 +9,14 @@ import android.widget.Toast;
 
 import java.util.List;
 
+
+// Reason why we need to use Interface to Communicate with from Fragment to Activity ....when we can do this stuff....with the help of Bundle, making it all static etc etc
+// here is the reason...to elemenate the problem to referesh ...suppose the task is to change text in fragment..n what we change in edit text in fragment shoud change the title of an Activity..(we do using textWatcher)
+// if we change edit text in fragment n send it to activity...it will not update at an instant..we have to referesh...
+//so interface communication will help...this is just a thinking...still need to implement that...go implement n let me know..
+
+
+
 public class MainActivity extends AppCompatActivity implements FirstFragment.OnFruitsItemSelected {
 
     @Override
